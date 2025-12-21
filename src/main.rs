052c630fa029caa44b2e36d3f09390caee24b5e0
Daniel Azevedo <49279228+dani-77@users.noreply.github.com>
2025-12-21 17:39:37 +0000

    Add EWMH support to Penrose window manager
    This commit adds EWMH support to the Penrose window manager, allowing interaction with external utilities. It includes key and mouse bindings, layout configuration, and initializes the window manager with the necessary hooks.

---
+++ src/main.rs
@@ -0,0 +1,170 @@
+//! penrose :: EWMH support
+//!
+//! It is possible to add EWMH support to penrose via an extension. This provides
+//! information to external utilities such as panels and statusbars so that they
+//! are able to interact with the window manager.
+//!
+//! `penrose::extensions::hooks::add_ewmh_hooks` can be used to compose the required
+//! hooks into your existing Config before starting the window manager. If you want
+//! to modify the support, each of the individual hooks can be found in
+//! `penrose::extensions::hooks::ewmh`.
+#[cfg(not(target_os = "macos"))]
+use penrose::x11rb::RustConn;
+use penrose::{
+    Result,
+    builtin::{
+        actions::{
+            exit,
+            floating::{MouseDragHandler, MouseResizeHandler, sink_focused},
+            log_current_state, modify_with, send_layout_message, spawn,
+        },
+        layout::{
+            MainAndStack,
+            messages::{ExpandMain, IncMain, ShrinkMain},
+            transformers::{Gaps, ReflectHorizontal, ReserveTop},
+        },
+    },
+    core::{
+        Config, WindowManager,
+        bindings::{
+            KeyEventHandler, MouseEventHandler, MouseState, click_handler,
+            parse_keybindings_with_xmodmap,
+        },
+        layout::LayoutStack,
+    },
+    extensions::{
+        actions::toggle_fullscreen,
+        hooks::{SpawnOnStartup, add_ewmh_hooks},
+    },
+    map, stack,
+};
+
+use penrose_ui::{bar::Position, core::TextStyle, status_bar};
+use std::collections::HashMap;
+use tracing_subscriber::{self, prelude::*};
+
+const FONT: &str = "Iosevka";
+const BLACK: u32 = 0x282828ff;
+const WHITE: u32 = 0xebdbb2ff;
+const GREY: u32 = 0x3c3836ff;
+const BLUE: u32 = 0x458588ff;
+const BAR_HEIGHT_PX: u32 = 20;
+
+#[cfg(not(target_os = "macos"))]
+fn raw_key_bindings() -> HashMap<String, Box<dyn KeyEventHandler<RustConn>>> {
+    let mut raw_bindings = map! {
+        map_keys: |k: &str| k.to_owned();
+
+        "M-j" => modify_with(|cs| cs.focus_down()),
+        "M-k" => modify_with(|cs| cs.focus_up()),
+        "M-S-j" => modify_with(|cs| cs.swap_down()),
+        "M-S-k" => modify_with(|cs| cs.swap_up()),
+        "M-q" => modify_with(|cs| cs.kill_focused()),
+        "M-Tab" => modify_with(|cs| cs.toggle_tag()),
+        "M-bracketright" => modify_with(|cs| cs.next_screen()),
+        "M-bracketleft" => modify_with(|cs| cs.previous_screen()),
+        "M-m" => modify_with(|cs| cs.next_layout()),
+        "M-S-m" => modify_with(|cs| cs.previous_layout()),
+        "M-Up" => send_layout_message(|| IncMain(1)),
+        "M-Down" => send_layout_message(|| IncMain(-1)),
+        "M-Right" => send_layout_message(|| ExpandMain),
+        "M-Left" => send_layout_message(|| ShrinkMain),
+        "M-a" => spawn("rofi -show drun"),
+        "M-x" => spawn("rofi -show power -modi power:rofi-power-menu"),
+        "M-p" => spawn("dmenu_run"),
+        "M-r" => spawn("gmrun"),
+        "M-S-s" => log_current_state(),
+        "M-Return" => spawn("st"),
+        "M-S-q" => exit(),
+        "M-S-f" => toggle_fullscreen(),
+    };
+
+    for tag in &["1", "2", "3", "4", "5", "6", "7", "8", "9"] {
+        raw_bindings.extend([
+            (
+                format!("M-{tag}"),
+                modify_with(move |client_set| client_set.focus_tag(tag)),
+            ),
+            (
+                format!("M-S-{tag}"),
+                modify_with(move |client_set| client_set.move_focused_to_tag(tag)),
+            ),
+        ]);
+    }
+
+    raw_bindings
+}
+
+#[cfg(not(target_os = "macos"))]
+fn mouse_bindings() -> HashMap<MouseState, Box<dyn MouseEventHandler<RustConn>>> {
+    use penrose::core::bindings::{
+        ModifierKey::{Meta, Shift},
+        MouseButton::{Left, Middle, Right},
+    };
+
+    map! {
+        map_keys: |(button, modifiers)| MouseState { button, modifiers };
+
+        (Left, vec![Shift, Meta]) => MouseDragHandler::boxed_default(),
+        (Right, vec![Shift, Meta]) => MouseResizeHandler::boxed_default(),
+        (Middle, vec![Shift, Meta]) => click_handler(sink_focused()),
+    }
+}
+
+pub fn layouts() -> LayoutStack {
+    let max_main = 1;
+    let ratio = 0.6;
+    let ratio_step = 0.1;
+    let outer_px = 5;
+    let inner_px = 5;
+    let top_px = 18;
+
+    stack!(
+        MainAndStack::side(max_main, ratio, ratio_step),
+        ReflectHorizontal::wrap(MainAndStack::side(max_main, ratio, ratio_step)),
+        MainAndStack::bottom(max_main, ratio, ratio_step)
+    )
+    .map(|layout| ReserveTop::wrap(Gaps::wrap(layout, outer_px, inner_px), top_px))
+}
+
+#[cfg(not(target_os = "macos"))]
+fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter("trace")
+        .finish()
+        .init();
+
+    // This is all that is needed to add the required hooks to your config.
+    // SpawnOnStartup is being used here to start polybar so that the EWMH support
+    // can be demonstrated.
+    let config = add_ewmh_hooks(Config {
+        default_layouts: layouts(),
+        startup_hook: Some(SpawnOnStartup::boxed("/etc/xdg/wmd77/startup.sh")),
+        ..Config::default()
+    });
+
+    let conn = RustConn::new()?;
+    let key_bindings = parse_keybindings_with_xmodmap(raw_key_bindings())?;
+    let style = TextStyle {
+        fg: WHITE.into(),
+        bg: Some(BLACK.into()),
+        padding: (2, 2),
+    };
+
+    let bar = status_bar(BAR_HEIGHT_PX, FONT, 10, style, BLUE, GREY, Position::Top).unwrap();
+
+   let wm = bar.add_to(WindowManager::new(
+        config,
+        key_bindings,
+        mouse_bindings(),
+        conn,
+    )?);
+
+    wm.run()
+
+}
+
+#[cfg(target_os = "macos")]
+fn main() -> Result<()> {
+    panic!("not supported on OSX");
+}
