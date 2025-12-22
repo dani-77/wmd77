2d2355259ed5769522119491256626fc880e8b94
Daniel Azevedo <daniazevedo77@posteo.net>
2025-12-22 00:20:22 +0000

    bar: weather & wifi

--- etc/xdg/wmd77/bar.sh
+++ etc/xdg/wmd77/bar.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
+#$(cat /sys/class/net/wl*/operstate 2>/dev/null)
 
 status() {
     # Date and time
     DATE=$(date '+%a %d %b %Y, %H:%M')
     
+    # Weather
+    WEATHER=$(curl wttr.in/?format=1 | awk '{ print $2 }')
+
     # Memory usage
     MEMORY=$(free -m | grep '^Mem' | awk '{print "Mem: " $3 "MB/" $2 "MB"}')
     
     # CPU usage
     CPU=$(top -bn1 | grep 'Cpu(s)' | sed 's/.*, *\([0-9.]*\)%* id.*/\1/' | awk '{print $1"%"}')
     
+    # Wlan
+	WLAN_STATE=$(cat /sys/class/net/wl*/operstate 2>/dev/null | head -1)
+    if [ "$WLAN_STATE" = "up" ]; then
+        WLAN="Connected"
+    else
+        WLAN="Disconnected"
+    fi
+
     # Volume
     VOLUME=$(amixer get Master | grep -o '[0-9]*%' | head -1)
     
@@ -17,7 +29,7 @@ status() {
     BATTERY=$(acpi -b | grep -o '[0-9]*%' | head -1)
     
     # Combine all info with prefixes
-    echo "CPU: $CPU | $MEMORY | Volume: $VOLUME | Battery: $BATTERY | $DATE"
+    echo "$WEATHER | CPU: $CPU | $MEMORY | $WLAN | Volume: $VOLUME | Battery: $BATTERY | $DATE"
 }
 
 while true; do
